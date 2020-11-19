e7cd7f663835f71c93520b9b260bfd7188c72e87
Bryce Kerley <bkerley@brycekerley.net>
2020-11-18 22:31:55 -0500

    getting ready to modularize

---
+++ src/lens.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const half_lens = new THREE.SphereBufferGeometry(15, 32, 16,
+                                                 0, Math.PI * 2,
+                                                 0, 0.5)
